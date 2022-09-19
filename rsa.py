e897e1c852d0ab90c80c2064a375060317d6bdb7
tanner-nordell <hnordell@seas.upenn.edu>
2022-09-19 11:19:21 -0400

    first push rsa

---
+++ rsa.py
@@ -0,0 +1,29 @@
+import random
+from rsa_util import mod_inverse
+from rsa_util import is_prime
+
+#The encryption exponent, e
+#Do not change this value
+e = 65537
+
+def RSAKeygen(bitlen):
+	n = 0
+	d = 0
+	while not is_prime(n):
+		n = random.SystemRandom().randint(1, pow(2, bitlen))
+	while not is_prime(d):
+		d = random.SystemRandom().randint(1, pow(2, bitlen))
+
+	return n,d
+
+def RSAEncrypt(n,m):
+	e = 65537
+	c = pow(m,e,n)
+	#Code goes here
+	return c
+
+def RSADecrypt(n,d,c):
+	m = pow(c,d,n)
+	#Code goes here
+	return m
+
