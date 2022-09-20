bf8e286d8f3d138bc901f3cc46597b12da0b8641
tanner-nordell <hnordell@seas.upenn.edu>
2022-09-19 20:50:17 -0400

    10th push rsa. Last changes fixed the test. Ironing out last few things.

--- rsa.py
+++ rsa.py
@@ -16,18 +16,18 @@ def RSAKeygen(bitlen):
 	#print("k: ",k)
 	while not is_prime(p):
 		p = random.SystemRandom().randint(pow(2,bitlen-1), pow(2, bitlen)-1)
-	#binaryp = bin(p)[2:]
-	#print("p # of bits= ", len(binaryp))
+	binaryp = bin(p)[2:]
+	print("p # of bits= ", len(binaryp))
 	while not is_prime(q):
 		#q = random.SystemRandom().randint(pow(2,(bitlen-k)-1), pow(2, bitlen-k)-1)
 		q = random.SystemRandom().randint(pow(2, bitlen - 1), pow(2, bitlen) - 1)
-	#binaryq = bin(q)[2:]
-	#print("q # of bits= ", len(binaryq))
+	binaryq = bin(q)[2:]
+	print("q # of bits= ", len(binaryq))
 	n=p*q
 
 	binary = bin(n)[2:]
 	print("bitlen: ", bitlen)
-	print("length= " , len(binary))
+	print("length of n= " , len(binary))
 	phiN = (p-1)*(q-1)
 	d = mod_inverse(e, phiN)
 	return n,d
